d30306819324578de2a753e9558f8f2b3a190837
Mariyam Salim <mariyamsalim03@gmail.com>
2026-03-30 01:05:46 +0400

    Cleaned up modal layout

--- script.js
+++ script.js
@@ -16,22 +16,37 @@ AFRAME.registerComponent('sinking-fish', {
 AFRAME.registerComponent('scanner', {
   init: function () {
     this.el.addEventListener('click', () => {
-      const fishName = this.el.getAttribute('data-name');
-      const fishInfo = this.el.getAttribute('data-info');
+      const name = this.el.getAttribute('data-name');
+      const sciName = this.el.getAttribute('data-sci');
+      const famName = this.el.getAttribute('data-fam');
+      const facts = this.el.getAttribute('data-facts');
+      const impact = this.el.getAttribute('data-impact');
+      const imgSrc = this.el.getAttribute('data-img');
 
-      const modal = document.querySelector('#scanner-modal');
-      const nameHeading = document.querySelector('#modal-name');
-      const infoText = document.querySelector('#modal-text');
+      const modal = document.getElementById('scanner-modal');
+      const factsContainer = document.getElementById('facts-container');
 
-      if (modal && nameHeading && infoText) {
-        nameHeading.innerText = fishName;
-        infoText.innerText = fishInfo;
-        
-        modal.style.display = 'block';
+      document.getElementById('modal-name').innerText = name || "Unknown";
+      document.getElementById('modal-sci').innerText = sciName || "N/A";
+      document.getElementById('modal-fam').innerText = famName || "N/A";
+      document.getElementById('modal-impact').innerText = impact || "No impact data available.";
+      const imgElement = document.getElementById('modal-img');
+      if (imgSrc) imgElement.src = imgSrc;
+      if (!facts || facts.trim() === "") {
+        factsContainer.style.display = 'none';
+      } else {
+        factsContainer.style.display = 'block';
+        document.getElementById('modal-facts').innerText = facts;
       }
+
+      modal.style.display = 'block';
     });
   }
 });
+
+window.closeModal = function() {
+  document.getElementById('scanner-modal').style.display = 'none';
+};
 
 function closeModal() {
   document.getElementById('scanner-modal').style.display = 'none';
